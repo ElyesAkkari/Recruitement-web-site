9a4052a6efad4f0c7d2cbd7d0fbe04344083487b
ElyesEsp <61230036+ElyesEsp@users.noreply.github.com>
2021-02-17 20:55:02 +0100

    Initial commit

---
+++ public/assets1/js/main.js
@@ -0,0 +1,108 @@
+(function($) {
+  
+  "use strict";  
+
+  $(window).on('load', function() {
+
+
+
+  /*Page Loader active
+    ========================================================*/
+    $('#preloader').fadeOut();
+
+  // Sticky Nav
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 50) {
+            $('.scrolling-navbar').addClass('top-nav-collapse');
+        } else {
+            $('.scrolling-navbar').removeClass('top-nav-collapse');
+        }
+    });
+
+    /* slicknav mobile menu active  */
+    $('.mobile-menu').slicknav({
+        prependTo: '.navbar-header',
+        parentTag: 'liner',
+        allowParentLinks: true,
+        duplicate: true,
+        label: '',
+        closedSymbol: '<i class="lni-chevron-right"></i>',
+        openedSymbol: '<i class="lni-chevron-down"></i>',
+      });
+
+      /* WOW Scroll Spy
+    ========================================================*/
+     var wow = new WOW({
+      //disabled for mobile
+        mobile: false
+    });
+
+    wow.init();
+
+     /* Testimonials Carousel 
+    ========================================================*/
+    var owl = $("#teams");
+      owl.owlCarousel({
+        loop: true,
+        nav: false,
+        dots: false,
+        center: true,
+        margin: 15,
+        slideSpeed: 1000,
+        stopOnHover: true,
+        autoPlay: true,
+        responsiveClass: true,
+        responsiveRefreshRate: true,
+        responsive : {
+            0 : {
+                items: 1
+            },
+            768 : {
+                items: 3
+            },
+            960 : {
+                items: 3
+            },
+            1200 : {
+                items: 3
+            },
+            1920 : {
+                items: 3
+            }
+        }
+      });  
+
+
+
+    /* Counter
+    ========================================================*/
+    $('.counterUp').counterUp({
+     delay: 10,
+     time: 1000
+    });
+
+
+    /* Back Top Link active
+    ========================================================*/
+      var offset = 200;
+      var duration = 500;
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+          $('.back-to-top').fadeIn(400);
+        } else {
+          $('.back-to-top').fadeOut(400);
+        }
+      });
+
+      $('.back-to-top').on('click',function(event) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+
+
+  });      
+
+}(jQuery));
